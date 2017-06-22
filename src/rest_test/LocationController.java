c0717278aed76c57efa4f5504e399e98dd520786
Shay Segal <SHAYSE@il.ibm.com>
2017-06-22 18:32:29 +0300

    new upload

--- src/rest_test/LocationController.java
+++ src/rest_test/LocationController.java
@@ -1,6 +1,8 @@
 package rest_test;
 
 import org.springframework.web.bind.annotation.*;
+
+import main.java.Exceptions.AlreadyExists;
 import main.java.Exceptions.LoginException;
 import main.java.Exceptions.NotExists;
 import main.java.data.management.DBManager;
@@ -9,6 +11,7 @@ import main.java.data.members.MapLocation;
 import main.java.data.members.ParkingArea;
 import main.java.data.members.ParkingAreas;
 import main.java.data.members.ParkingSlot;
+import main.java.data.members.ParkingSlotStatus;
 import main.java.data.members.User;
 import main.java.logic.Navigation;
 import java.util.ArrayList;
@@ -68,12 +71,37 @@ public class LocationController {
 			System.out.println("exception...");
 		}
 	}
+
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/Locations", produces = "application/json")
 	@ResponseBody
 	public String getLocations() {
 		setUpLocations();
 		return new JSONObject(hmap) + "";
+	}
+	
+	@CrossOrigin(origins = "http://localhost:8100")
+	@RequestMapping(value = "/Floors", produces = "application/json")
+	@ResponseBody
+	public String getFloors() {
+		setUpLocations();
+		Map<String, List<Map<String, String>>> floors = new HashMap<>();
+		for (String key : hmap.keySet())
+			if (!"Ulman".equals(key))
+				floors.put(key, new ArrayList<>());
+			else {
+				List<Map<String, String>> ulman = new ArrayList<>();
+				Map<String, String> floor1 = new HashMap<>();
+				floor1.put("description", "take the stairs");
+				floor1.put("id", "Floor 1");
+				Map<String, String> floor2 = new HashMap<>();
+				floor2.put("description", "take the elevator");
+				floor2.put("id", "Floor 2");
+				ulman.add(floor1);
+				ulman.add(floor2);
+				floors.put(key, ulman);
+			}
+		return new JSONObject(floors) + "";
 	}
 
 	@RequestMapping(value = "/Locations/{name}", produces = "application/json")
@@ -98,12 +126,13 @@ public class LocationController {
 				return ps;
 		return null;
 	}
+
 	@CrossOrigin(origins = "http://localhost:8100")
 	@RequestMapping(value = "/ParkingAreas", produces = "application/json")
 	@ResponseBody
 	public Set<ServerParkingArea> getParkingAreas() {
 		parkingAreas = new HashSet<ServerParkingArea>();
-		 for (ParkingArea a : areas.getParkingAreas())
+		for (ParkingArea a : areas.getParkingAreas())
 			parkingAreas.add(new ServerParkingArea(a));
 		return parkingAreas;
 	}
@@ -122,10 +151,20 @@ public class LocationController {
 	public void findBestPark(@RequestParam("car") String carNumber, @RequestParam("src") String src,
 			@RequestParam("dest") String dest) {
 		try {
-			sps = new ServerParkingSlot(Navigation.closestParkingSlot(new User(carNumber),
-					new Destination(src).getEntrance(), areas, new Destination(dest)));
+			User u = new User(carNumber);
+			sps = null;
+
+			if (u.getCurrentParking() != null)
+				throw new AlreadyExists("You have a parking slot");
+
+			u.addToLastPaths(src, dest);
+
+			sps = new ServerParkingSlot(
+					Navigation.closestParkingSlot(u, new Destination(src).getEntrance(), areas, new Destination(dest)));
 		} catch (ParseException | LoginException | NotExists e) {
 			System.out.println("exception...");
+		} catch (AlreadyExists e) {
+			System.out.println((e + ""));
 		}
 	}
 
@@ -135,4 +174,18 @@ public class LocationController {
 		return sps != null ? sps : null;
 	}
 
+	@RequestMapping(value = "/LeavePark", method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	public void leavePark(@RequestParam("car") String carNumber) {
+		try {
+			User u = new User(carNumber);
+			u.getCurrentParking().changeStatus(ParkingSlotStatus.FREE);
+			u.setCurrentParking(null);
+			sps = null;
+
+		} catch (LoginException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
 }
